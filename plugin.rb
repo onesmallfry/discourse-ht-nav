9bd4597f3ffda0cd28445c555e4e1519bf7bd13d
Ankur Pandit <ankur@onesmallfry.com>
2019-12-05 15:55:27 -0600

    Add plugin.rb and connectors.

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-ht-nav
+# about: Load header and footer from main HT site
+# version: 0.0.1
+# authors: Ankur Pandit <ankur@onesmallfry.com>
+
+register_asset "javascripts/discourse/templates/connectors/above-site-header/ht-header.hbs"
+register_asset "javascripts/discourse/templates/connectors/above-footer/ht-footer.hbs"
