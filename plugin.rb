988e580f7fcdfc510fa8f6a04feb73edac6d749b
Ankur Pandit <ankur@onesmallfry.com>
2020-01-17 14:07:21 -0600

    Change asset extensions for discourse build process.

--- plugin.rb
+++ plugin.rb
@@ -5,5 +5,5 @@
 
 register_asset "javascripts/discourse/templates/connectors/above-site-header/ht-header.hbs"
 register_asset "javascripts/discourse/templates/connectors/above-footer/ht-footer.hbs"
-register_asset "javascripts/discourse/initializers/ht-scripts.js"
-register_asset "stylesheets/ht-styles.css"
+register_asset "javascripts/discourse/initializers/ht-scripts.js.es6"
+register_asset "stylesheets/ht-styles.scss"
